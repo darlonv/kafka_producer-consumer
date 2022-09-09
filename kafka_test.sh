07941656ce26e091895b15c057c900e336a56cdb
Darlon <darlonv@gmail.com>
2022-09-09 00:25:21 -0300

    unfinished.

--- kafka_test.sh
+++ kafka_test.sh
@@ -1,14 +1,37 @@
+#!/bin/bash
+
+#Debug scripting. The recommendation is not to use it =/
+
+TOPIC='hellowz'
+MESSAGE='Hello world'
+
+WAIT='3s'
+
+docker-compose up -d
+
+echo "Waiting $WAIT until cluster up"
+sleep $WAIT
 
 #Create topic
-kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic 'hello'
+echo "Creating topic $TOPIC :"
+docker-compose exec kafka kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic $TOPIC
 
 #List topics
-kafka-topics.sh --list --bootstrap-server kafka:9092
-
-#Producer
-kafka-console-producer.sh --request-required-acks 1 --broker-list kafka:9092 --topic 'hello'
+echo 'Listing topics:'
+docker-compose exec kafka kafka-topics.sh --list --bootstrap-server kafka:9092
 
 #Consumer
-kafka-console-consumer.sh --bootstrap-server kafka:9092 --topic 'hello'
+echo 'Opening consumer:'
+docker-compose exec kafka -it kafka-console-consumer.sh --bootstrap-server kafka:9092 --topic 'hello' &
+
+sleep $WAIT
+
+#Producer
+echo "Producing message "
+docker-compose exec kafka -it  kafka-console-producer.sh --request-required-acks 1 --broker-list kafka:9092 --topic $TOPIC <<< $MESSAGE
+
+
+
+docker-compose down
 
 
